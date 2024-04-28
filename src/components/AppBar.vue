b97a685e06db091b95829065479b12b96039f61e
Aleksa Zdravković <aleksazdravkovic@proton.me>
2024-04-28 21:16:55 +0200

    Dodat IconButton i rekonstruisan kod

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,21 +1,17 @@
 <template>
   <div
-    class="flex justify-between items-center py-2 px-3 text-primary bg-background font-areal font-bold text-2xl sticky"
+    class="flex justify-between items-center py-2 px-3 text-primary bg-background font-areal font-bold text-2xl sticky top-0"
   >
     <h1>{{ $route.meta.naslov }}</h1>
 
     <div class="flex justify-center items-center">
       <!-- Dobrodosli -->
       <RouterLink :to="{ name: 'dobrodosli' }">
-        <div class="flex justify-center p-1 mr-2 cursor-pointer">
-          <i class="fa-regular fa-circle-question"></i>
-        </div>
+        <IconButton icon="fa-regular fa-circle-question" class="mr-6" />
       </RouterLink>
       <!-- Informacije -->
       <RouterLink :to="{ name: 'informacije' }">
-        <div class="flex justify-center p-1 w-7 cursor-pointer">
-          <i class="fa-solid fa-ellipsis-vertical"></i>
-        </div>
+        <IconButton icon="fa-solid fa-ellipsis-vertical" />
       </RouterLink>
     </div>
   </div>
@@ -23,4 +19,5 @@
 
 <script setup>
 import { RouterLink } from 'vue-router'
+import IconButton from './IconButton.vue'
 </script>
---
+++ src/components/IconButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex justify-center p-1 cursor-pointer text-primary text-2xl">
+    <i :class="props.icon"></i>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({ icon: String })
+</script>
