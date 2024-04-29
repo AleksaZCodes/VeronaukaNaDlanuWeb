d5676c1c99203957f268bb6bdf67ae81f9bb3c7a
Aleksa Zdravković <aleksazdravkovic@proton.me>
2024-04-29 13:42:36 +0200

    Dodata kartica za predstavljanje

---
+++ src/components/CardFeatured.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <CardPlain class="gap-2">
+      <div class="flex flex-row items-baseline gap-2">
+        <i class="text-primary text-base" :class="props.icon"></i>
+        <label class="italic text-sm">{{ label }}</label>
+      </div>
+      <slot></slot>
+      <RouterLink :to="{ name: link }" class="text-primary italic underline text-sm">{{
+        linkText
+      }}</RouterLink>
+    </CardPlain>
+  </div>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+import CardPlain from './CardPlain.vue'
+
+const props = defineProps({ icon: String, label: String, link: String, linkText: String })
+</script>
