3c7c2d5c608233751db4f3b2f89378dea46f52d0
Aleksa Zdravković <aleksazdravkovic@proton.me>
2024-04-29 16:49:16 +0200

    Ispravljen bug sa bojom ikonice

--- src/components/BottomNavigationBar.vue
+++ src/components/BottomNavigationBar.vue
@@ -2,10 +2,16 @@
   <div class="sticky bottom-0">
     <div class="h-4 bg-gradient-to-t from-background to-transparent"></div>
     <div class="flex justify-around items-center w-screen py-3 bg-background">
-      <RouterLink v-for="r in sortRoutes($router.getRoutes())" :key="r.name" :to="{ name: r.name }">
+      <RouterLink
+        v-for="r in sortRoutes($router.getRoutes())"
+        :key="r.indeks"
+        :to="{ name: r.name }"
+      >
         <IconButton
-          :icon="r.meta.ikonica"
-          :class="r.meta.naslov === $route.meta.naslov ? 'text-primary' : 'text-outlineVariant'"
+          :icon="[
+            r.meta.ikonica,
+            r.meta.naslov === $route.meta.naslov ? 'text-primary' : 'text-outlineVariant'
+          ]"
         />
       </RouterLink>
     </div>
