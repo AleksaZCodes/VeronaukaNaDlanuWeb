1110bb38a041bb1f4806e7e986fba51ce1bdc085
Aleksa Zdravković <aleksazdravkovic@proton.me>
2024-04-29 16:48:56 +0200

    Uklonjen margin

--- src/components/CardPlain.vue
+++ src/components/CardPlain.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-card rounded-xl shadow px-3 py-2 mx-3 flex flex-col">
+  <div class="bg-card rounded-xl shadow px-3 py-2 flex flex-col">
     <slot></slot>
   </div>
 </template>
