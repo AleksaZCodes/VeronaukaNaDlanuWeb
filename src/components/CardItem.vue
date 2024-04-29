9e15b1fb0f8ce5fe5aeb252798fc378c2c311f17
Aleksa Zdravković <aleksazdravkovic@proton.me>
2024-04-29 17:54:03 +0200

    Dodat CardItem

---
+++ src/components/CardItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <CardPlain class="mx-3">
+      <div class="grid grid-cols-10 items-center">
+        <div class="flex flex-col col-span-9">
+          <h2 class="text-primary font-bold">{{ props.title }}</h2>
+          <label class="italic text-sm truncate">{{ props.text }}</label>
+        </div>
+        <div>
+          <slot></slot>
+        </div>
+      </div>
+    </CardPlain>
+  </div>
+</template>
+
+<script setup>
+import CardPlain from '@/components/CardPlain.vue'
+
+const props = defineProps({ title: String, text: String })
+</script>
