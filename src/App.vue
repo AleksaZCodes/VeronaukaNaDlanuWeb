76e22f08bedb8f178624db3a7415923870545366
Aleksa Zdravković <aleksazdravkovic@proton.me>
2024-04-30 18:07:10 +0200

    Rekonstruisanje

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="flex flex-col h-screen justify-between">
-    <AppBar :title="$route.meta.naslov" v-if="$route.meta.appBar !== undefined">
-      <!-- Dobrodosli -->
-      <RouterLink :to="{ name: 'dobrodosli' }">
-        <IconButton icon="fa-regular fa-circle-question" class="mr-3" />
-      </RouterLink>
-      <!-- Informacije -->
-      <RouterLink :to="{ name: 'informacije' }">
-        <IconButton icon="fa-solid fa-ellipsis-vertical"
-      /></RouterLink>
-    </AppBar>
-    <RouterView class="flex-auto" />
+    <div>
+      <AppBar :title="$route.meta.naslov" v-if="$route.meta.appBar !== undefined">
+        <!-- Dobrodosli -->
+        <RouterLink :to="{ name: 'dobrodosli' }">
+          <IconButton icon="fa-regular fa-circle-question" class="mr-3" />
+        </RouterLink>
+        <!-- Informacije -->
+        <RouterLink :to="{ name: 'informacije' }">
+          <IconButton icon="fa-solid fa-ellipsis-vertical"
+        /></RouterLink>
+      </AppBar>
+      <RouterView />
+    </div>
     <BottomNavigationBar v-if="$route.meta.bottomNavigationBar !== undefined" />
   </div>
 </template>
