1733b73e61acb2e89a578089fea9d21f54d96e49
Aleksa Zdravković <aleksazdravkovic@proton.me>
2024-05-01 21:39:58 +0200

    Rekonstruisano

--- src/views/Molitve/CardMolitva.vue
+++ src/views/Molitve/CardMolitva.vue
@@ -1,9 +1,9 @@
 <template>
   <CardPlain>
     <div class="flex flex-col gap-3 p-1">
-      <h1 class="text-xl font-bold text-primary">{{ props.molitva.naslov }}</h1>
-      <p class="whitespace-pre text-wrap tracking-wide">{{ props.molitva.telo }}</p>
-      <label class="text-primary italic">Извор: {{ props.molitva.izvor }}</label>
+      <h1 class="text-xl font-bold text-primary">{{ molitva.naslov }}</h1>
+      <p class="whitespace-pre text-wrap tracking-wide">{{ molitva.telo }}</p>
+      <label class="text-primary italic">Извор: {{ molitva.izvor }}</label>
     </div>
   </CardPlain>
 </template>
@@ -11,5 +11,5 @@
 <script setup>
 import CardPlain from '@/components/CardPlain.vue'
 
-const props = defineProps({ molitva: Object })
+defineProps({ molitva: Object })
 </script>
