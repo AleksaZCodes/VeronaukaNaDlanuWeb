c9512522d38657841bcbe6578d7b1854bbc0c1ac
Aleksa Zdravković <aleksazdravkovic@proton.me>
2024-05-01 20:30:11 +0200

    Rekonstruisano

---
+++ src/components/BottomBar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="sticky bottom-0">
+    <div class="h-4 bg-gradient-to-t from-background to-transparent"></div>
+    <div class="flex justify-around items-center w-screen py-3 bg-background">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
--- src/components/BottomNavigationBar.vue
+++ src/components/BottomNavigationBar.vue
@@ -1,25 +1,19 @@
 <template>
-  <div class="sticky bottom-0">
-    <div class="h-4 bg-gradient-to-t from-background to-transparent"></div>
-    <div class="flex justify-around items-center w-screen py-3 bg-background">
-      <RouterLink
-        v-for="r in sortRoutes($router.getRoutes())"
-        :key="r.indeks"
-        :to="{ name: r.name }"
-      >
-        <IconButton
-          :icon="
-            r.meta.ikonica +
-            ' ' +
-            (r.meta.naslov === $route.meta.naslov ? 'text-primary' : 'text-outlineVariant')
-          "
-        />
-      </RouterLink>
-    </div>
-  </div>
+  <BottomBar>
+    <RouterLink v-for="r in sortRoutes($router.getRoutes())" :key="r.indeks" :to="{ name: r.name }">
+      <IconButton
+        :icon="
+          r.meta.ikonica +
+          ' ' +
+          (r.meta.naslov === $route.meta.naslov ? 'text-primary' : 'text-outlineVariant')
+        "
+      />
+    </RouterLink>
+  </BottomBar>
 </template>
 
 <script setup>
+import BottomBar from './BottomBar.vue'
 import IconButton from './IconButton.vue'
 
 const sortRoutes = (routes) => {
